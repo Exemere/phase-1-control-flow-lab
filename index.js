a0b84d49e03407518c88da3163508289a21d2670
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-11 12:02:12 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+const scuberGreetingForFeet= 199;
+if (scuberGreetingForFeet<= 400)  {
+   console.log('This one is on me')
+}else if(scuberGreetingForFeet>400 && 199<= 2000){
+     console.log('Your charge is 30$!')
+}else (scuberGreetingForFeet<400 && ride> 2500);
+    console.log('No ride available')
